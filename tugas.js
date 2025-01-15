dd58e23f84322caad89c9701497a2bb43afbee5a
ekaputri5209 <ekaputrifebriyanti99@gmail.com>
2025-01-15 11:13:12 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,44 @@
+let namaDepan = "Eka";
+let namaTengah = "Putri";
+let namaBelakang = "Febriyanti";
+
+document.write("nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang);
+let alamat= "Rusunawa kudu";
+
+document.write(" saya tinggal di rusunawa kudu");
+let usia = "15 tahun";
+
+document.write(" pada tahun 2024 ini usia saya adalah 15 tahun.");
+let ttl =(" semarang 5 februari 2009");
+
+document.write(" saya lahir di semarang 5 februari 2009");
+let gender =" perempuan";
+
+document.write(" jenis kelamin saya adalah perempuan ");
+let agama = " islam "
+
+document.write(" saya beragama islam ");
+let email = " ekaputri99@gmail.com";
+
+document.write(" alamat email saya adalah ekaputri99@gmail.com");
+let berat =" 60 kg";
+
+document.write(" saya memiliki berat badan 60 kg");
+let tiggi = " 155cm";
+
+document.write(" dengan tinggi 155 cm");
+let hobi =" membaca";
+
+document.write(" saya sangat senang menyanyi");
+let cita =" dokter hewan";
+
+document.write(" karea saya memiliki cita cita dokter hewan");
+let  pekerjaan = " pelajar";
+
+document.write(" saat ini saya adalah seorang pelajar");
+let sekolah =" SMKN 1 SAYUNG";
+
+document.write(" yang bersekolah di SMKN 1 SAYUNG");
+let jurusan =" PPLG";
+
+document.write(" pada jurusan PPLG");
